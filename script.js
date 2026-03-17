73ab87e5e5733fd780818f0844d2d30436a4524a
AlexanderYepez-code <alexsaomn17@gmail.com>
2026-03-17 16:47:47 +0100

    esercizio completo senza bonus

--- script.js
+++ script.js
@@ -8,22 +8,45 @@
 // Utilizzerai Promise.all() per eseguire queste richieste in parallelo e poi restituirai un oggetto con i dati aggregati.
 
 
-const getDashboardData = async (query) =>{
-    const res = await fetch(`http://localhost:3333/destinations?search=${query}`)
-    const obj = await res.json()
-    const destination = obj[0];
+// const getDashboardData = async (query) =>{
+//     const res = await fetch(`http://localhost:3333/destinations?search=${query}`)
+//     const obj = await res.json()
+//     const destination = obj[0];
 
-    const data = {
-        city: destination.name,
-        country: destination.country,
+//     const data = {
+//         city: destination.name,
+//         country: destination.country,
+//     }
+// return data
+// }
+
+
+
+
+    async function getDashboardData(query) {
+        console.log(`caricando i dati dalla chiamata ${query}`)
+        try{
+            const destinationPromise = fetchRequest(`http://localhost:3333/destinations?search=${query}`);
+            const weatherPromise = fetchRequest(`http://localhost:3333/weathers?search=${query}`);
+            const airportspromise = fetchRequest(`http://localhost:3333/airports?search=${query}`);
+
+            const promises = [destinationPromise,weatherPromise,airportspromise];
+            const[destination,weather,airport] = await Promise.all(promises);
+
+            return {
+                city: destination[0].name,
+                country: destination[0].country,
+                temperature : weather[0].temperature,
+                weather_description: weather[0].weather_description,
+                airport: airport[0].name 
+
+            }
+        }catch(error){
+            throw new Error(`Errore nel recupero dei dati`)
+        }
+
+        
     }
-return data
-}
-
-
-
-
-
 
 
 getDashboardData('london')
@@ -36,3 +59,12 @@ getDashboardData('london')
         );
     })
     .catch(error => console.error(error));
+
+    async function fetchRequest(url) {
+        const response = await fetch (url);
+        const obj = await response.json()
+        return obj
+    }
+
+
+
